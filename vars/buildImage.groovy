ed0b39a039de70fd7ee70fa091733f4d79bfb5bb
aydamacink <dayanamick@gmail.com>
2024-11-02 17:10:33 +0100

    call docker imageName with param, instead of hardcoded

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
-def call() {
+def call(String imageName) {
     echo 'building the docker image'
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t dm1984/demo-app:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push dm1984/demo-app:jma-2.0'
+        sh "docker push $imageName"
     }  
 }
