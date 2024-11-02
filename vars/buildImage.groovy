755ac3d8663c893cfaabd704d89e31c245e6f937
aydamacink <dayanamick@gmail.com>
2024-11-02 16:20:38 +0100

    undo some changes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,6 +1,8 @@
-#!/user/bin/env groovy
-
-import com.example.Docker
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+def call() {
+    echo 'building the docker image'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+        sh 'docker build -t dm1984/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push dm1984/demo-app:jma-2.0'
+    }  
 }
